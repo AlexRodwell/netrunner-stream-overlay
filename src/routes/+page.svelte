201f614b8c0b29b5545f9a82d383aeb2ddad80b5
AlexR <AlexRodwell@users.noreply.github.com>
2023-10-09 14:00:53 +0100

    feat: added home page for quick navigation when first accessing the app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<a class="button" href="/dashboard">Dashboard</a>
+
+<a class="button" href="/overlay">Overlay</a>
+<a class="button" href="/overlay/timer">Timer</a>
+<a class="button" href="/overlay/player/one">Player One</a>
+<a class="button" href="/overlay/player/two">Player Two</a>
+
+<style lang="scss">
+	:global(body) {
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+		background-color: #121212;
+	}
+</style>
