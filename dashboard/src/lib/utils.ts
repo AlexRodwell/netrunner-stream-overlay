d1f60df639e113921ffa6026ce74b559f438588f
AlexR <AlexRodwell@users.noreply.github.com>
2024-01-14 14:46:46 +0000

    refactor: add shadcdn-svelte utility functions to existing utils file

--- dashboard/src/lib/utils.ts
+++ dashboard/src/lib/utils.ts
@@ -1,3 +1,70 @@
+// Shadcn sVELTE
+
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { cubicOut } from "svelte/easing";
+import type { TransitionConfig } from "svelte/transition";
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
+}
+
+type FlyAndScaleParams = {
+	y?: number;
+	x?: number;
+	start?: number;
+	duration?: number;
+};
+
+export const flyAndScale = (
+	node: Element,
+	params: FlyAndScaleParams = { y: -8, x: 0, start: 0.95, duration: 150 }
+): TransitionConfig => {
+	const style = getComputedStyle(node);
+	const transform = style.transform === "none" ? "" : style.transform;
+
+	const scaleConversion = (
+		valueA: number,
+		scaleA: [number, number],
+		scaleB: [number, number]
+	) => {
+		const [minA, maxA] = scaleA;
+		const [minB, maxB] = scaleB;
+
+		const percentage = (valueA - minA) / (maxA - minA);
+		const valueB = percentage * (maxB - minB) + minB;
+
+		return valueB;
+	};
+
+	const styleToString = (
+		style: Record<string, number | string | undefined>
+	): string => {
+		return Object.keys(style).reduce((str, key) => {
+			if (style[key] === undefined) return str;
+			return str + `${key}:${style[key]};`;
+		}, "");
+	};
+
+	return {
+		duration: params.duration ?? 200,
+		delay: 0,
+		css: (t) => {
+			const y = scaleConversion(t, [0, 1], [params.y ?? 5, 0]);
+			const x = scaleConversion(t, [0, 1], [params.x ?? 0, 0]);
+			const scale = scaleConversion(t, [0, 1], [params.start ?? 0.95, 1]);
+
+			return styleToString({
+				transform: `${transform} translate3d(${x}px, ${y}px, 0) scale(${scale})`,
+				opacity: t,
+			});
+		},
+		easing: cubicOut,
+	};
+};
+
+// Custom
+
 import JSON_FACTIONS from "$lib/data/factions.json";
 import JSON_COUNTRIES from "world_countries_lists/data/countries/en/countries.json";
 import { netrunnerDB } from "./store";
@@ -78,7 +145,7 @@ export const fetch_cards = async () => {
 		const response = await fetch(api.endpoint + api.cards);
 
 		netrunnerDB.subscribe((value) => {
-			console.log(value);
+			// console.log(value);
 		});
 
 		if (!response.ok) {
