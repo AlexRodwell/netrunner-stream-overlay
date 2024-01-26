09f2cbee0c6cabc700fd4ec58a8fb1fb0f6bb5c3
AlexR <AlexRodwell@users.noreply.github.com>
2024-01-26 23:50:30 +0000

    Fix typo in component import, refactor CSS to tailwind
    fix: incorrect spelling of components
    style(refactor): overlay page to use tailwind

--- dashboard/src/routes/overlay/+page.svelte
+++ dashboard/src/routes/overlay/+page.svelte
@@ -1,23 +1,20 @@
 <script lang="ts">
 	import { netrunnerDB, playerOneData, playerTwoData } from "$lib/store";
 	import { default as PlayerMeta } from "$components/overlay/Meta.svelte";
-	import CardHightlight from "$components/overlay/CardHighlight.svelte";
+	import CardHighlight from "$components/overlay/CardHighlight.svelte";
 	import Loading from "$components/Loading.svelte";
 	import Commentators from "$components/overlay/Commentators.svelte";
 </script>
 
 {#if $netrunnerDB && $playerOneData && $playerTwoData}
 	<main class="wrapper">
-		<!-- <h1 style="position: absolute; top: 1rem; left: 1rem; color: #fff;">
-			commentators: {$globalData.overlay.commentators}
-		</h1> -->
 		<Commentators />
-		<CardHightlight
+		<CardHighlight
 			player="playerOne"
 			data={$playerOneData.highlight}
 			side={$playerOneData.side}
 		/>
-		<CardHightlight
+		<CardHighlight
 			player="playerTwo"
 			data={$playerTwoData.highlight}
 			side={$playerTwoData.side}
@@ -25,7 +22,9 @@
 		<PlayerMeta />
 	</main>
 {:else}
-	<main class="wrapper wrapper--loading">
+	<main
+		class="wrapper flex place-content-center place-items-center flex-col gap-4"
+	>
 		<Loading />
 		<p>Awaiting data...</p>
 	</main>
@@ -33,28 +32,6 @@
 
 <style lang="scss">
 	.wrapper {
-		display: grid;
-		grid-template-columns: repeat(2, minmax(0, 1fr));
-		grid-template-rows: 1fr auto;
-		width: 100vw; //  var(--width, 100vw);
-		height: 100vh; //  var(--height, 100vh);
-		overflow: hidden;
-		position: relative;
-
-		&--loading {
-			display: flex;
-			place-content: center;
-			place-items: center;
-			flex-direction: column;
-			gap: 1rem;
-		}
-
-		&__overlay {
-			position: absolute;
-			width: 100%;
-			height: 100%;
-			object-fit: fill;
-			z-index: -1;
-		}
+		@apply grid grid-cols-2 grid-rows-[1fr_auto] w-screen h-screen overflow-hidden relative;
 	}
 </style>
