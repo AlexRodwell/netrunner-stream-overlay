93d42faae3f8c87d4b4797a90d34fcb9a919e726
AlexR <AlexRodwell@users.noreply.github.com>
2024-01-26 23:35:41 +0000

    feat: commit missing i18n translation initialisation function

--- dashboard/src/routes/+layout.ts
+++ dashboard/src/routes/+layout.ts
@@ -1,1 +1,10 @@
+import { loadTranslations } from "$lib/translations";
+
+/** @type {import('@sveltejs/kit').Load} */
+export const load = async () => {
+	await loadTranslations("en");
+
+	return {};
+};
+
 export const prerender = true;
