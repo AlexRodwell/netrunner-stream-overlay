6c3e138219fbc7fd6ed92128ed46fabba55a6c98
AlexR <AlexRodwell@users.noreply.github.com>
2024-01-27 00:06:48 +0000

    feat: conditional display commentator (only display if it has a value)

--- dashboard/src/lib/components/overlay/Commentators.svelte
+++ dashboard/src/lib/components/overlay/Commentators.svelte
@@ -4,8 +4,10 @@
 	$: data = $globalData;
 </script>
 
-<div
-	class="p-3 bg-white text-black absolute top-4 left-2/4 -translate-x-1/2 text-[1.5rem]"
->
-	{data.overlay.commentators}
-</div>
+{#if data.overlay.commentators}
+	<div
+		class="p-3 bg-white text-black absolute top-4 left-2/4 -translate-x-1/2 text-[1.5rem]"
+	>
+		{data.overlay.commentators}
+	</div>
+{/if}
