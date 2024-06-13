ef2d87f91bd1caab4b91d257e29eddf47ca00b09
AlexR <AlexRodwell@users.noreply.github.com>
2024-06-13 23:26:56 +0100

    style: refactor root/index page button layout

--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
 	import { Button } from "$lib/components/ui/button";
+	import { t } from "$lib/translations";
 </script>
 
-<main class="w-screen h-screen flex flex-col justify-center items-center gap-4">
-	<div class="grid grid-cols-4 gap-4">
-		<Button variant="outline" href="/dashboard">Dashboard</Button>
-		<Button variant="outline" href="/overlay">Overlay</Button>
-		<Button variant="outline" href="/overlay/commentators"
-			>Commentators</Button
-		>
-		<Button variant="outline" href="/overlay/timer">Timer</Button>
+<main class="bg-[#101010] text-foreground w-screen h-screen flex flex-col justify-center items-center gap-4">
+	<div class="grid grid-cols-2 gap-4 w-[40vw] h-[25vh]">
+		<Button variant="outline" class="w-full h-full col-span-full" align="center" href="/dashboard">{$t('dashboard')}</Button>
+		<Button variant="outline" class="w-full h-full" align="center" href="/overlay">{$t('overlay_aio')}</Button>
+		<Button variant="outline" class="w-full h-full" align="center" href="/overlay/commentators">{$t('commentators')}</Button>
+		<Button variant="outline" class="w-full h-full" align="center" href="/overlay/timer">{$t('timer')}</Button>
 	</div>
 </main>
